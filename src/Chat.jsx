c410c15f6df78823b5c759724929098c401c31dc
Abhishek Kumar <abhishek3kumar@gmail.com>
2025-07-25 08:23:03 +0530

    feat: Point API to live Render backend

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -22,7 +22,7 @@ function Chat() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/chat', {
+      const response = await fetch('https://careersum-backend.onrender.com/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: currentInput } ),
